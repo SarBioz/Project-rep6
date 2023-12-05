558611d43f1e17e669fc876b5c268c00b150c741
SarBioz <151008316+SarBioz@users.noreply.github.com>
2023-12-05 06:13:38 -0500

    Update expression-bias.r

--- expression-bias.r
+++ expression-bias.r
@@ -42,3 +42,34 @@ ggplot(sum_data, aes(x = Column_Name, y = Sum_Value)) +
        x = "Column Name",
        y = "Sum Value") +
   theme_minimal()
+#------------------------------------------------------------------------
+calculate_ratio <- function(pair) {
+  gene1 <- pair[1]
+  gene2 <- pair[2]
+  ratio <- log2(x_data[gene1, ] / x_data[gene2, ])
+  return(ratio)
+}
+ratio_data <- apply(merged_data, 1, calculate_ratio)
+combined_df <- do.call(rbind, ratio_data)
+combined_df$gene_pairs <- merged_data$sorted_pair
+
+last_column <- ncol(combined_df)
+
+# Set the last column as row names
+row.names(combined_df) <- combined_df[, last_column]
+
+# Remove the last column (if you want)
+combined_df <- combined_df[, -last_column]
+
+
+binary_df <- ifelse(combined_df >= -2 & combined_df <= 2, 1, 0)
+column_sums <- colSums(binary_df, na.rm = TRUE)
+sum_data <- data.frame(Column_Name = names(column_sums), Sum_Value = column_sums)
+library(ggplot2)
+
+ggplot(sum_data, aes(x = Column_Name, y = Sum_Value)) +
+  geom_bar(stat = "identity", fill = "skyblue", color = "black") +
+  labs(title = "Sum of Values in Each Column",
+       x = "Column Name",
+       y = "Sum Value") +
+  theme_minimal()
