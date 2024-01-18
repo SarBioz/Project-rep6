d7d591462eb3e7dd2f49aac06cae0aa250e3acac
SarBioz <151008316+SarBioz@users.noreply.github.com>
2024-01-18 03:05:12 -0500

    Update average expression.r

--- average expression.r
+++ average expression.r
@@ -23,50 +23,52 @@ obj <- RunUMAP(obj, dims = 1:20, reduction = "pca", reduction.name = "umap.unint
 umap_coords <- as.data.frame(Embeddings(obj, reduction = "umap.unintegrated")[, 1:2])
 clusters <- as.numeric(Idents(obj))
 
-# Filter the Seurat object to include only cells where cluster_species is "human_10x"
-# Subset based on species
-human_10x_data <- subset(obj, species_tech == "human_10x")
-# Calculate average expression within each cluster for the filtered data
-human_10x_cluster_means <- Seurat::AverageExpression(human_10x_data, by.group = "clusters")
-human_10x_cluster_means_df <- as.data.frame(human_10x_cluster_means)
-# Print the result
-print(human_10x_cluster_means)
+# Access gene expression data for each cluster
+cluster_gene_expression <- human_10x_data@assays$RNA@data
+# Accessing cluster information
+cluster_info <- human_10x_data$cluster
+# changing the colnames to the cluster 
+colnames(cluster_gene_expression) <- cluster_info
 
-# 3) genes expressed in more than 1 and more than 2 cell after clustring
+binary_table2 <- rowSums(cluster_gene_expression > 1)
+genes_at_least1 <-  rownames(cluster_gene_expression)[binary_table2 >= 1]
+genes_at_least_2 <- rownames(cluster_gene_expression)[binary_table2 >= 2]
 
-genes_in_seurat <- rownames(human_10x_cluster_means_df)
-genepir_new <- genepair %>% select(name, paralogue_name)
-human_10x_cluster_means_df$gene_name_column_in_obj <- rownames(human_10x_cluster_means_df)
-human_10x_cluster_means_df_name <- human_10x_cluster_means_df %>% select(gene_name_column_in_obj)
-filtered_gene_pairs_data <- genepir_new[genepair$name %in% genes_in_seurat, ]
-
-# Merge gene pairs data with Seurat object based on gene names
-merged_data <- merge(x = human_10x_cluster_means_df_name, y = filtered_gene_pairs_data, by.x = "gene_name_column_in_obj", by.y = "name")
-x_data <-human_10x_cluster_means_df[, -ncol(human_10x_cluster_means_df)]
-value_data <- ifelse(x_data > 1, 1, 0)
-# Calculate row sums
-row_sums <- rowSums(value_data)
-# Identify rows with sum greater than 1
-more_than_1_rows <- names(row_sums[row_sums > 1])
-# Create a new dataframe
-more_than_1 <- data.frame(RowName = more_than_1_rows)
-# Identify rows with sum greater than 2
-more_than_2_rows <- names(row_sums[row_sums > 2])
-# Create a new dataframe
-more_than_2 <- data.frame(RowName = more_than_2_rows)
-
+Genes_At_Least_1Cell_name <- data.frame(
+  Genes_At_Least_1Cell = genes_at_least1)
+Genes_At_Least_2Cells_name <- data.frame(
+  Genes_At_Least_2Cells = genes_at_least_2)
 
 genes_at_least_one <- Genes_At_Least_One_Cell_name$Genes_At_Least_One_Cell
-more_than_one <- more_than_1$RowName
+more_than_one <- Genes_At_Least_1Cell_name$Genes_At_Least_1Cell
 genes_at_least_two <- Genes_At_Least_Two_Cells_name$Genes_At_Least_Two_Cells
-more_than_two <- more_than_2$RowName
+more_than_two <- Genes_At_Least_2Cells_name$Genes_At_Least_2Cells
 
-
+library(VennDiagram)
 
 y = list(genes_at_least_two, more_than_two , genes_at_least_one , more_than_one )
 venn.plot <- venn.diagram(
   x = y ,
   category.names = c("at_least2_before" , "at_least2_after " , "at_least1_before" , "at_least1_after " ),
+  filename = NULL,
+  output=TRUE
+)
+
+grid.draw(venn.plot)
+
+
+venn.plot <- venn.diagram(
+  x = list(genes_at_least_two, more_than_two  ) ,
+  category.names = c("at_least2_before" , "at_least2_after "),
+  filename = NULL,
+  output=TRUE
+)
+
+grid.draw(venn.plot)
+
+venn.plot <- venn.diagram(
+  x = list(genes_at_least_one , more_than_one ) ,
+  category.names = c("at_least1_before","at_least1_after "),
   filename = NULL,
   output=TRUE
 )
